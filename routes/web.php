f5442595e05a5ed076d72bb2fd51b46b4d0b4830
PedroIlustre <pedro.ilustre.web@gmail.com>
2020-09-18 19:36:07 -0300

    creating a laravel project

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+    //Route::get('/', 'BookController@index'); -- doesn't work
+    Route::get('/', 'App\Http\Controllers\BookController@index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
